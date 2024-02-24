1fc220f854bcec3a2298999ac1fce30d8c83a874
BSMITSME <sun4461637@gmail.com>
2024-02-24 22:24:11 +0900

    climbing

---
+++ climbingtheleaderboard/Solution.java
@@ -0,0 +1,75 @@
+package climbingtheleaderboard;
+
+import static java.util.stream.Collectors.toList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+import javax.management.openmbean.ArrayType;
+import javax.print.DocFlavor.INPUT_STREAM;
+
+class Result {
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        // Write your code here
+
+        Map<Integer, Integer> rankMap = new HashMap<>();
+        List<Integer> arr = new ArrayList<>();
+    
+        int counter = 1;
+        for (int i = 0; i<player.size(); i++ ){
+            
+            ranked.add(player.get(i));
+            Collections.sort(ranked, Collections.reverseOrder());
+            for (int a = 0; a<ranked.size(); a++){
+                if (!rankMap.containsKey(ranked.get(a))){
+                    rankMap.put(ranked.get(a), counter++);
+                }
+            }
+            
+            System.out.println("rankMap"+rankMap);
+            arr.add(rankMap.get(player.get(i)));
+
+        }
+
+        return arr;
+    }
+}
+public class Solution{
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int rankedCount = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        List<Integer> ranked = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+        
+        int playerCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> player = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.climbingLeaderboard(ranked, player);
+        
+        System.out.println(result);
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
